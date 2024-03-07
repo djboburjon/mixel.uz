43dd4fc1414a3acf11aec03cce9938d6f241b755
djboburjon <djboburdev@gmail.com>
2024-03-07 14:22:18 +0500

    href added

--- src/components/brands/Brands.jsx
+++ src/components/brands/Brands.jsx
@@ -4,21 +4,21 @@ import "./Brands.css";
 function Brands() {
   return (
     <div className="brends">
-      <div>
-        <img src="./imgs/canon.png" alt="" />
-      </div>
-      <div>
-        <img src="./imgs/mi.png" alt="" />
-      </div>
-      <div>
-        <img src="./imgs/lg.png" alt="" />
-      </div>
-      <div>
-        <img src="./imgs/samsung.png" alt="" />
-      </div>
-      <div>
-        <img src="./imgs/artel.png" alt="" />
-      </div>
+      <a href="https://www.usa.canon.com/" target="_blank">
+        <img src="./imgs/canon.png" alt="canon logo" />
+      </a>
+      <a href="https://www.mi.com/us/" target="_blank">
+        <img src="./imgs/mi.png" alt="mi logo" />
+      </a>
+      <a href="https://www.lg.com/us" target="_blank">
+        <img src="./imgs/lg.png" alt="lg logo" />
+      </a>
+      <a href="https://www.samsung.com/uz_ru/" target="_blank">
+        <img src="./imgs/samsung.png" alt="samsung logo" />
+      </a>
+      <a href="https://artelgroup.org/" target="_blank">
+        <img src="./imgs/artel.png" alt="artel logo" />
+      </a>
     </div>
   );
 }

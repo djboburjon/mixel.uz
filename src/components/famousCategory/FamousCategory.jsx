adb95b0539802b0a082a35c39fd8d2845de52f99
djboburjon <djboburdev@gmail.com>
2024-03-07 14:01:46 +0500

    link added to famousCategory

--- src/components/famousCategory/FamousCategory.jsx
+++ src/components/famousCategory/FamousCategory.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import "./FamousCategory.css";
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 // import required modules
-import { Pagination, Navigation } from 'swiper/modules';
+import { Pagination, Navigation } from "swiper/modules";
+import { Link } from "react-router-dom";
 
 function FamousCategory() {
   return (
@@ -24,22 +25,39 @@ function FamousCategory() {
       className="mySwiper"
     >
       <SwiperSlide className="famous">
-        <h2>Компьютеры</h2> <img src="./imgs/famous.png" alt="" />
+        <Link to={"products/all"}>
+          <h2>Наши магазины</h2> <img src="./imgs/famous.png" alt="" />
+        </Link>
       </SwiperSlide>
       <SwiperSlide className="famous">
-        <h2>Компьютеры</h2> <img src="./imgs/famous.png" alt="" />
+        <Link to={"products/phones"}>
+          <h2>Телефоны, планшеты</h2> <img src="./imgs/phones.png" alt="" />
+        </Link>
       </SwiperSlide>
       <SwiperSlide className="famous">
-        <h2>Компьютеры</h2> <img src="./imgs/famous.png" alt="" />
+        <Link to={"products/laptops"}>
+          <h2>Ноутбуки</h2> <img src="./imgs/laptops.png" alt="" />
+        </Link>
       </SwiperSlide>
       <SwiperSlide className="famous">
-        <h2>Компьютеры</h2> <img src="./imgs/famous.png" alt="" />
+        <Link to={"products/accessories"}>
+          <h2>Комплектующие</h2> <img src="./imgs/office.png" alt="" />
+        </Link>
       </SwiperSlide>
       <SwiperSlide className="famous">
-        <h2>Компьютеры</h2> <img src="./imgs/famous.png" alt="" />
+        <Link to={"products/networks"}>
+          <h2>Сетевое оборудование</h2> <img src="./imgs/famous.png" alt="" />
+        </Link>
       </SwiperSlide>
       <SwiperSlide className="famous">
-        <h2>Компьютеры</h2> <img src="./imgs/famous.png" alt="" />
+        <Link to={"products/monoblock"}>
+          <h2>Моноблоки</h2> <img src="./imgs/famous.png" alt="" />
+        </Link>
+      </SwiperSlide>
+      <SwiperSlide className="famous">
+        <Link to={"products/equipments"}>
+          <h2>Оргтехника</h2> <img src="./imgs/famous.png" alt="" />
+        </Link>
       </SwiperSlide>
     </Swiper>
   );

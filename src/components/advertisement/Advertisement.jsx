7c85662a3f32f8b998c8c39cce380bde9254eb69
djboburjon <djboburdev@gmail.com>
2024-03-12 14:26:09 +0500

    autoplay time extended

--- src/components/advertisement/Advertisement.jsx
+++ src/components/advertisement/Advertisement.jsx
@@ -18,7 +18,7 @@ function Advertisement() {
           spaceBetween={30}
           centeredSlides={true}
           autoplay={{
-            delay: 2500,
+            delay: 5000,
             disableOnInteraction: false,
           }}
           pagination={{
--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -16,7 +16,7 @@ function Banner() {
       spaceBetween={30}
       centeredSlides={true}
       autoplay={{
-        delay: 2500,
+        delay: 5000,
         disableOnInteraction: false,
       }}
       pagination={{
